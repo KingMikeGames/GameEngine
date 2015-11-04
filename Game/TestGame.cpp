1de77b2a7e94075d9d78cf339890e1caa8e1254d
MikeRemedios <michael.remedios2@hotmail.com>
2015-11-04 01:03:38 -0500

    anime

--- Game/TestGame.cpp
+++ Game/TestGame.cpp
@@ -3,6 +3,10 @@
 
 void TestGame::Init()
 {
+	/*
+
+	MANUAL CUBE
+
 	 Vertex vertices[8] = {
 		Vertex(glm::vec3(-0.5, -0.5, 0.5), glm::vec2(0,0), glm::vec3(0,0,0), glm::vec4(1,0,0,1)),
 		Vertex(glm::vec3(-0.5, 0.5, 0.5), glm::vec2(0,0), glm::vec3(0,0,0), glm::vec4(0,1,0,1)),
@@ -22,19 +26,23 @@ void TestGame::Init()
 		2,5,3,5,7,1,
 		4,3,7,3,0,7
 	};
+	*/
 
-	m_mesh = new Mesh(vertices, 8, indices, 36, false);
-	m_texture = new Texture("animu_tex.jpg"); // not being applied in the shader
+	m_mesh = new Mesh("res/models/crate.obj");
+	m_texture = new Texture("animu_tex.jpg"); 
+	m_texture1 = new Texture("crate_tex.jpg");
 	m_material = Material(m_texture, glm::vec3(1, 1, 1));
+	m_material1 = Material(m_texture1, glm::vec3(1, 1, 1));
 
 	m_meshRenderer = new MeshRenderer(*m_mesh, m_material);
+	m_meshRenderer1 = new MeshRenderer(*m_mesh, m_material1);
 
 	
 	m_cube.AddComponent(m_meshRenderer);
 	m_cube.GetTransform().setPosition(glm::vec3(0, 0, 3));
-
-	m_cube1.AddComponent(m_meshRenderer);
-	m_cube1.GetTransform().setPosition(glm::vec3(0, 1, 0));
+	
+	m_cube1.AddComponent(m_meshRenderer1);
+	m_cube1.GetTransform().setPosition(glm::vec3(1, 1, 0));
 	m_cube1.GetTransform().setRotation(glm::angleAxis(45.f, glm::vec3(0, 1, 0)));
 	
 	m_cube.AddChild(m_cube1);
--- Game/TestGame.h
+++ Game/TestGame.h
@@ -18,6 +18,9 @@ private:
 
 	Mesh* m_mesh;
 	Material m_material;
+	Material m_material1;
 	Texture* m_texture;
+	Texture* m_texture1;
 	MeshRenderer* m_meshRenderer;
+	MeshRenderer* m_meshRenderer1;
 };
