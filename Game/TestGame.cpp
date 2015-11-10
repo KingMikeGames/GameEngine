a28eecadfcfd4b000c2f70d518fa1ea38f2a9051
King Arthur <tylerklimke@gmail.com>
2015-11-10 17:38:02 -0500

    s

--- Game/TestGame.cpp
+++ Game/TestGame.cpp
@@ -2,18 +2,31 @@
 #include <vector>
 void TestGame::Init()
 {
-	m_mesh = new Mesh("./res/models/crate.obj");
+	m_mesh = new Mesh("./res/models/neptune.obj");
 
 	
-	mats.emplace_back(new Texture("crate.jpg"), glm::vec3(1.0f, 1.0f, 1.0f));
+	mats.emplace_back(new Texture("neptune/1.jpg"), glm::vec3(1.0f, 1.0f, 1.0f));
+	mats.emplace_back(new Texture("neptune/2.jpg"), glm::vec3(1.0f, 1.0f, 1.0f));
+	mats.emplace_back(new Texture("neptune/4.jpg"), glm::vec3(1.0f, 1.0f, 1.0f));
+	mats.emplace_back(new Texture("neptune/5.jpg"), glm::vec3(1.0f, 1.0f, 1.0f));
 	m_meshRenderer = new MeshRenderer(*m_mesh, mats);
 
-	m_cube.AddComponent(m_meshRenderer);
-	m_cube.GetTransform().setPosition(glm::vec3(0, 0, 3));
-	m_cube.GetTransform().setScale(glm::vec3(1.0f,1.0f,1.0f));
+	mats1.emplace_back(new Texture("neptune/1.jpg"), glm::vec3(1.0f, 1.0f, 1.0f));
+	mats1.emplace_back(new Texture("neptune/2.jpg"), glm::vec3(1.0f, 1.0f, 1.0f));
+	mats1.emplace_back(new Texture("neptune/3.jpg"), glm::vec3(1.0f, 1.0f, 1.0f));
+	mats1.emplace_back(new Texture("neptune/5.jpg"), glm::vec3(1.0f, 1.0f, 1.0f));
+	m_meshRenderer1 = new MeshRenderer(*m_mesh, mats1);
+
+	m_neptune.AddComponent(m_meshRenderer);
+	m_neptune.GetTransform().setPosition(glm::vec3(-8, 0, 15));
+	m_neptune.GetTransform().setScale(glm::vec3(0.1f,0.1f,0.1f));
 	
-	AddToScene(m_cube);
-	//AddToScene(m_cube1);
+	m_neptune1.AddComponent(m_meshRenderer1);
+	m_neptune1.GetTransform().setPosition(glm::vec3(8, 0, 15));
+	m_neptune1.GetTransform().setScale(glm::vec3(0.1f, 0.1f, 0.1f));
+
+	AddToScene(m_neptune);
+	AddToScene(m_neptune1);
 }
 
 void TestGame::Update(float delta)
@@ -21,7 +34,8 @@ void TestGame::Update(float delta)
 	static float temp = 0;
 	temp += delta;
 
-	m_cube.GetTransform().setRotation(glm::angleAxis(temp, glm::vec3(0, 1, 0)));
+	m_neptune.GetTransform().setRotation(glm::angleAxis(temp, glm::vec3(1, 0, 0)));
+	m_neptune1.GetTransform().setRotation(glm::angleAxis(temp, glm::vec3(-1, 0, 0)));
 }
 
 TestGame::~TestGame()
--- Game/TestGame.h
+++ Game/TestGame.h
@@ -13,10 +13,13 @@ private:
 	TestGame(const TestGame& other) {}
 	void operator=(const TestGame& other) {}
 
-	GameObject m_cube;
+	GameObject m_neptune;
+	GameObject m_neptune1;
 
 	Mesh* m_mesh;
 	std::vector<Material> mats;
+	std::vector<Material> mats1;
 	Texture* m_texture;
 	MeshRenderer* m_meshRenderer;
+	MeshRenderer* m_meshRenderer1;
 };
